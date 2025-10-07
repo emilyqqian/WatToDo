05f29e75d0f5ca24025b393c48ab9b436fbb5b74
ofts-cqm <qianmuchen.sam@gmail.com>
2025-10-07 16:38:35 -0400

    set-up basic framework

--- Project/src/code.py
+++ Project/src/code.py
@@ -8,6 +8,8 @@ import argparse
 # because sometimes the table is "Data", sometims it is "TestTable"
 TABLE = "Data"
 
+########## Logging In Functions - These are functions that handles database connections ##########
+
 # this function creates a connection to the target database
 # with specified username and password
 def createConnection(user, password):
@@ -50,6 +52,31 @@ def init():
     global cursor
     (database, cursor) = temp_connection
     print("Connection Established")
+    
+########## Helper Functions - These are functions that might be useful for the six functions or testcases ##########
+    
+# this is a helper function that checks if a task is valid
+# for the six functions, consider calling this to check if the input is valid
+def assertTaskValid(task, allowNullDoneDate = False):
+    # check if input is valid
+    if not(len(task) == 5 or (allowNullDoneDate and len(task) == 4)):
+        raise ValueError(f"Incorrect number of variables: 5{(' or 4' if allowNullDoneDate else '')} required, found {len(task)}")
+
+    if type(task[0]) != str:
+        raise ValueError(f"'Item' must be a string!")
+
+    if type(task[1]) != str:
+        raise ValueError(f"'Type' must be a string!")
+
+    if type(task[2]) != datetime:
+        raise ValueError(f"'started' must be a valid datetime!")
+
+    if type(task[3]) != datetime:
+        raise ValueError(f"'due' must be a valid datetime!")
+    elif task[3] < task[2]:
+        raise ValueError(f"'due' must be later than 'started'!")
+    elif task[3] < datetime.now():
+        raise ValueError(f"'due' must be later than current time!")
     
 if __name__ == "__main__":
     init()
