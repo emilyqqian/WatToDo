70af34630ba14a19dcadc259da15e710e17fe203
ofts-cqm <qianmuchen.sam@gmail.com>
2025-10-28 18:29:21 -0400

    update leaderboard functions

--- Project/src/database.h
+++ Project/src/database.h
@@ -13,7 +13,7 @@ class User{
     unsigned int userid;
     std::string username;// username has to be unique
     unsigned int password;// hash of password
-    unsigned int xpPoint;
+	unsigned int points = 0;// points the user has earned
 
     // used internally
     User(unsigned int userid, std::string username, unsigned int password, unsigned int xpPoint);
@@ -93,4 +93,8 @@ DatabaseResult addUserToTaskboard(unsigned int user_id, unsigned int taskboard_i
 DatabaseResult kickOutUserFromTaskboard(unsigned int user_id, unsigned int taskboard_id);
 DatabaseResult deleteTaskBoard(TaskBoard taskboard);
 
+// functions about leaderboard
+std::unordered_map<User, unsigned int> getLeaderboard();// I don't think theres a need to use DatabaseResult here, since this function will always succeed
+// if you want to set xp, just call updateUserInfo
+// if you want to get xp, just call getLeaderboard() and find the user. The cost are negligible
 #endif
