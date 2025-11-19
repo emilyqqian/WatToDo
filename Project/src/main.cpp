515b68f5a8fd4329eb3b4ac0f27595b07811689a
ofts-cqm <qianmuchen.sam@gmail.com>
2025-11-18 21:27:14 -0500

    fix api issue

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -565,7 +565,6 @@ int main() {
                 int i = 0;
                 for (const auto& taskboard : taskboards) {
                     // DO NOT CHANGE THIS!!!!! @Yash
-                    std::cout << "Recovered " << taskboard.tasklist.size() << " Tasks for " << taskboard.name << '\n';
                     response["taskboards"][i] = getTaskBoardJSON(taskboard);
                     i++;
                 }
